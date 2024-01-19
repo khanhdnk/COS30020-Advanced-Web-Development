4ef612af7bddfeb691dc43c300860765b7c30219
khanhdnk <dangnamkhanhkdgv@gmail.com>
2024-01-19 16:49:40 +0700

    u

---
+++ Assignment1/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Job Vacancy Posting System</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="postjobform.php">Post job</a></li>
+            <li><a href="searchjobform.php">Search job</a></li>
+            <li><a href="about.php">About assignment</a></li>
+        </ul>
+    </nav>
+    <div>
+        <p>Name: Dang Nam Khanh</p>
+        <p>Student ID: 104225661</p>
+        <p>Email: <a href="">104225661@student.swin.edu.au</a></p>
+        <br>
+        <p>I declare that this assignment is my individual work. I have not worked collaboratively nor have I copied from any other student’s work or from any other source.</p>
+        <a href="">Post a job vacancy</a>
+        <a href="">Search for a job vacacy</a>
+        <a href="">About this assignment</a>
+    </div>
+    <?php
+        
+    ?>
+</body>
+</html>
