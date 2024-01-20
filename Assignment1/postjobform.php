4646b7c130bcbdfb2bb52c30b2a0161df579ed85
khanhdnk <121726197+khanhdnk@users.noreply.github.com>
2024-01-20 15:13:53 +0700

    u

--- Assignment1/postjobform.php
+++ Assignment1/postjobform.php
@@ -22,34 +22,44 @@
         <form action="" method="POST">
             <label for="positionID">Position ID:</label>
             <input type="text" name="positionID" id="positionID">
+
             <br>
             <label for="title">Title:</label>
             <input type="text" name="title" id="title">
+
             <br>
             <label for="description">Description:</label>
             <input type="text" name="description" id="description">
+
             <br>
             <label for="closingDate">Closing Date</label>
             <input type="text" name="closingDate" id="closingDate">
+
             <br>
             <span>Position:</span>
             <input type="radio" name="fullTime" id="fullTime">
             <label for="fullTime">Full Time</label>
+
             <input type="radio" name="partTime" id="partTime">
             <label for="partTime">Part Time</label>
+
             <br>
             <span>Contract:</span>
             <input type="radio" name="onGoing" id="onGoing">
             <label for="onGoing">On-going</label>
+            
             <input type="radio" name="fixedTerm" id="fixedTerm">
             <label for="fixedTerm">Fixed Term</label>
+
             <br>
             <span>Application by:</span>
             <input type="textbox" name="post" id="post">
             <label for="post">Post</label>
+
             <input type="textbox" name="mail" id="mail">
             <label for="mail">Mail</label>
             <br>
+
             <label for="location">Location:</label>
             <select id="location" name="location">
                 <option value="none">---</option>
@@ -58,6 +68,7 @@
                 <option value="saigon">Saigon</option>
                 <!-- Add more options as needed -->
             </select>
+            
             <br>
             <input type="submit" value="Post">
             <input type="reset" value="Reset">
---
+++ Assignment1/searchjobform.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Job Vacancy Posting System</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="postjobform.php">Post job</a></li>
+            <li><a href="searchjobform.php">Search job</a></li>
+            <li><a href="about.php">About assignment</a></li>
+        </ul>
+    </nav>
+
+    <div class="container">
+        <h1>Job Vacancy Posting System</h1>
+        <label for="jobTitle">Job Title</label>
+        <input type="text" name="jobTitle" id="jobTitle">
+        <input type="submit" value="search">
+        <br>
+        <a href="index.php">Return to Home Page</a>
+    </div>
+</body>
+
+</html>
