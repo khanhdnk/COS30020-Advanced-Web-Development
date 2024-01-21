d0a868cf97d8ca27b838157b7c36329144aa8560
khanhdnk <dangnamkhanhkdgv@gmail.com>
2024-01-21 19:28:09 +0700

    u

--- Assignment1/postjobprocess.php
+++ Assignment1/postjobprocess.php
@@ -23,11 +23,9 @@
     {
         if (!isset($value) || empty($value)) {
             echo "<p>Please provide the $name.</p>";
-        }
-        else if (!preg_match($pattern, $value)) {
+        } else if (!preg_match($pattern, $value)) {
             echo "<p>$error</p>";
-        }
-        else{
+        } else {
             return $value;
         }
         return null;
@@ -37,41 +35,58 @@
     {
         // If the file doesn't exist, we can assume the position ID is unique
         if (!file_exists($pathToFile)) {
+            echo"this is not exist";
             return true;
         }
-    
+
         $fileHandle = fopen($pathToFile, 'r');
-    
+
         // If the file couldn't be opened, throw an exception
         if (!$fileHandle) {
             throw new Exception("Unable to open file: $pathToFile");
         }
-    
+
         while (($line = fgets($fileHandle)) !== false) {
             $attributes = explode("\t", $line);
             $existingPositionId = isset($attributes[0]) ? trim($attributes[0]) : null;
-    
+
             if ($existingPositionId === $positionId) {
                 fclose($fileHandle);
                 return false; // Position ID already exists
             }
         }
-    
+
         fclose($fileHandle);
         return true; // Position ID is unique
     }
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $title = validateInputField('title', $_POST['title'], '/^[\p{L}0-9 ,.!]{1,20}$/u', 'Title must only contain letters, numbers, and spaces.');
-        if (!$title){
-            echo "shit";
+        $title = validateInputField('title', $_POST['title'], '/^[\p{L}0-9 ,.!]{1,20}$/u', 'Title must only contain letters (maximum 20 characters), numbers, and spaces.');
+        if (!$title) {
+            echo "this is null";
+        }
+        if (isPositionIdUnique($_POST['positionID'], 'jobposts/jobs.txt')) {
+            echo "this is unique";
+        } else {
+            echo "this is not unique";
         }
     }
 
 
 
     ?>
-    
+    <div class="lastnote">
+        <p class="return"><a href="postjobform.php"><span>Back to Job Posting Page </span></a></p>
+        <p class="return"><a href="index.php"><span>Back to Home</span></a></p>
+    </div>
+
+
+
+
+
+
+
+
 </body>
 
 </html>
