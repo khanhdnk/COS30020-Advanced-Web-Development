0435e3d4cbd529cf7702b99d84a93706ea81c872
khanhdnk <121726197+khanhdnk@users.noreply.github.com>
2024-01-21 11:31:24 +0700

    u

---
+++ Assignment1/postjobprocess.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Job Vacancy Posting System</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="postjobform.php">Post job</a></li>
+            <li><a href="searchjobform.php">Search job</a></li>
+            <li><a href="about.php">About assignment</a></li>
+        </ul>
+    </nav>
+
+    <?php
+    function validateInputField($name, $value, $pattern, $error)
+    {
+        if (!isset($value) || empty($value)) {
+            return "<p>Please provide the $name.</p>";
+        } else {
+            return true;
+        }
+    }
+
+    ?>
+</body>
+
+</html>
