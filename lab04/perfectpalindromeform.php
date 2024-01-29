a7cdfeb988819ec5a77a1a0e33c00f38b6e05131
khanhdnk <121726197+khanhdnk@users.noreply.github.com>
2024-01-30 00:37:54 +0700

    u

---
+++ lab04/perfectpalindromeform.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="perfectpalindrome.php" method="POST">
+        <label for="str">Enter string</label>
+        <input type="text" name="str" id="str">
+        <input type="submit" value="Check Palindrome">
+    </form>
+</body>
+</html>
---
+++ lab04/strform.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="utf-8" />
+ <meta name="description" content="Web application development" />
+ <meta name="keywords" content="PHP" />
+ <meta name="author" content="Your Name" />
+ <title>TITLE</title>
+</head>
+<body>
+<h1>Web Programming Form - Lab 4</h1>
+<form action="strprocess.php" method = "POST">
+    <p><label for="str">Enter a string:</label>
+    <input type="text" name="str" id="str" /></p>
+    <p><input type="submit" value="Submit" /></p>
+</form>
+</body>
+</html>
