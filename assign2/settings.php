24b372aa7131aabc2f12c638021ee4e4ba352eb9
khanhdnk <dangnamkhanhkdgv@gmail.com>
2024-03-17 15:39:25 +0700

    u

---
+++ assign2/settings.php
@@ -0,0 +1,14 @@
+<?php
+$host = "feenix-mariadb.swin.edu.au";
+$user = "s104225661"; // your user name
+$pswd = "280504"; // your password d(date of birth – ddmmyy)
+$dbnm = "s104225661_db"; // your database
+?>
+
+<?php
+//    $host = "localhost";
+//    $user = "root"; // your user name
+//    $pswd = ""; // your password d(date of birth – ddmmyy)
+//    $dbnm = "s104225661_db"; // your database
+//
+//?>
