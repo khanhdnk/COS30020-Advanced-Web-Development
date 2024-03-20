fb0c1d3204c802232960e6631678db7d717624b3
khanhdnk <121726197+khanhdnk@users.noreply.github.com>
2024-03-20 22:45:41 +0700

    u

--- assign2/index.php
+++ assign2/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,17 +9,17 @@
     <link rel="stylesheet" href="style.css">
     <title>Home</title>
 </head>
+
 <body>
     <nav class=" shadow-lg shadow-gray-300 w-100 px-8 md:px-auto">
         <div class="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
             <!-- Logo -->
             <div class="text-indigo-500 md:order-1">
-                <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-                     width="40pt" height="40pt" viewBox="0 0 512.000000 512.000000"
-                     preserveAspectRatio="xMidYMid meet">
+                <svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="40pt" height="40pt"
+                    viewBox="0 0 512.000000 512.000000" preserveAspectRatio="xMidYMid meet">
 
-                    <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
-                       fill="#000000" stroke="none">
+                    <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" fill="#000000"
+                        stroke="none">
                         <path d="M105 5015 l-25 -24 0 -2191 0 -2191 25 -24 24 -25 1709 0 c1668 0
                             1710 0 1733 19 23 18 24 26 29 176 l5 157 60 23 60 23 415 -413 c275 -274 430
                             -421 460 -436 70 -36 177 -34 248 5 61 34 130 105 163 167 33 62 33 175 0 239
@@ -34,54 +35,54 @@
                             -169 0 -316 51 -444 155 -174 140 -268 335 -268 557 0 265 137 497 372 628
                             150 83 326 106 510 66z m1150 -1953 c45 -67 42 -157 -8 -214 -15 -17 -40 6
                             -403 369 l-387 387 57 47 c31 26 62 55 70 64 13 16 42 -10 328 -296 173 -172
-                            327 -333 343 -357z"/>
+                            327 -333 343 -357z" />
                         <path d="M640 4640 l0 -80 80 0 80 0 0 -183 0 -183 -58 -57 c-53 -52 -61 -57
                             -103 -57 -42 0 -49 4 -109 65 -35 36 -69 65 -75 65 -14 0 -105 -92 -105 -106
                             0 -6 40 -49 89 -97 l88 -87 114 0 113 0 103 103 103 103 0 217 0 217 80 0 80
-                            0 0 80 0 80 -240 0 -240 0 0 -80z"/>
+                            0 0 80 0 80 -240 0 -240 0 0 -80z" />
                         <path d="M1504 4706 c-99 -32 -190 -128 -214 -228 -7 -28 -11 -102 -8 -185 3
                             -122 6 -144 27 -188 32 -63 92 -124 156 -155 41 -20 65 -25 135 -25 70 0 94 5
                             135 25 64 31 124 91 155 155 24 47 25 60 25 215 0 155 -1 168 -25 215 -32 64
                             -92 124 -155 154 -61 29 -170 37 -231 17z m189 -179 c55 -42 67 -79 67 -207 0
                             -129 -12 -165 -68 -208 -29 -22 -46 -27 -92 -27 -46 0 -63 5 -92 27 -59 45
-                            -70 81 -66 222 3 115 5 125 30 158 55 73 152 88 221 35z"/>
+                            -70 81 -66 222 3 115 5 125 30 158 55 73 152 88 221 35z" />
                         <path d="M2105 4695 l-25 -24 0 -351 0 -351 25 -24 c24 -25 26 -25 216 -25
                             230 0 256 6 324 75 87 87 100 201 33 298 -19 27 -19 27 0 55 64 92 54 204 -25
                             292 -65 72 -97 80 -330 80 -193 0 -194 0 -218 -25z m430 -160 c30 -29 32 -74
                             6 -106 -18 -23 -26 -24 -160 -27 l-141 -4 0 81 0 81 135 0 c131 0 137 -1 160
                             -25z m0 -320 c30 -29 32 -74 6 -106 -18 -23 -26 -24 -160 -27 l-141 -4 0 81 0
-                            81 135 0 c131 0 137 -1 160 -25z"/>
-                        <path d="M400 3680 l0 -80 640 0 640 0 0 80 0 80 -640 0 -640 0 0 -80z"/>
-                        <path d="M400 3360 l0 -80 1440 0 1440 0 0 80 0 80 -1440 0 -1440 0 0 -80z"/>
-                        <path d="M400 3040 l0 -80 1440 0 1440 0 0 80 0 80 -1440 0 -1440 0 0 -80z"/>
-                        <path d="M400 2720 l0 -80 160 0 160 0 0 80 0 80 -160 0 -160 0 0 -80z"/>
-                        <path d="M880 2720 l0 -80 560 0 560 0 0 80 0 80 -560 0 -560 0 0 -80z"/>
-                        <path d="M400 2400 l0 -80 160 0 160 0 0 80 0 80 -160 0 -160 0 0 -80z"/>
-                        <path d="M880 2400 l0 -80 560 0 560 0 0 80 0 80 -560 0 -560 0 0 -80z"/>
-                        <path d="M400 2080 l0 -80 160 0 160 0 0 80 0 80 -160 0 -160 0 0 -80z"/>
-                        <path d="M880 2080 l0 -80 560 0 560 0 0 80 0 80 -560 0 -560 0 0 -80z"/>
-                        <path d="M400 1760 l0 -80 160 0 160 0 0 80 0 80 -160 0 -160 0 0 -80z"/>
-                        <path d="M880 1760 l0 -80 560 0 560 0 0 80 0 80 -560 0 -560 0 0 -80z"/>
-                        <path d="M400 1440 l0 -80 160 0 160 0 0 80 0 80 -160 0 -160 0 0 -80z"/>
-                        <path d="M880 1440 l0 -80 560 0 560 0 0 80 0 80 -560 0 -560 0 0 -80z"/>
-                        <path d="M400 1120 l0 -80 160 0 160 0 0 80 0 80 -160 0 -160 0 0 -80z"/>
-                        <path d="M880 1120 l0 -80 560 0 560 0 0 80 0 80 -560 0 -560 0 0 -80z"/>
+                            81 135 0 c131 0 137 -1 160 -25z" />
+                        <path d="M400 3680 l0 -80 640 0 640 0 0 80 0 80 -640 0 -640 0 0 -80z" />
+                        <path d="M400 3360 l0 -80 1440 0 1440 0 0 80 0 80 -1440 0 -1440 0 0 -80z" />
+                        <path d="M400 3040 l0 -80 1440 0 1440 0 0 80 0 80 -1440 0 -1440 0 0 -80z" />
+                        <path d="M400 2720 l0 -80 160 0 160 0 0 80 0 80 -160 0 -160 0 0 -80z" />
+                        <path d="M880 2720 l0 -80 560 0 560 0 0 80 0 80 -560 0 -560 0 0 -80z" />
+                        <path d="M400 2400 l0 -80 160 0 160 0 0 80 0 80 -160 0 -160 0 0 -80z" />
+                        <path d="M880 2400 l0 -80 560 0 560 0 0 80 0 80 -560 0 -560 0 0 -80z" />
+                        <path d="M400 2080 l0 -80 160 0 160 0 0 80 0 80 -160 0 -160 0 0 -80z" />
+                        <path d="M880 2080 l0 -80 560 0 560 0 0 80 0 80 -560 0 -560 0 0 -80z" />
+                        <path d="M400 1760 l0 -80 160 0 160 0 0 80 0 80 -160 0 -160 0 0 -80z" />
+                        <path d="M880 1760 l0 -80 560 0 560 0 0 80 0 80 -560 0 -560 0 0 -80z" />
+                        <path d="M400 1440 l0 -80 160 0 160 0 0 80 0 80 -160 0 -160 0 0 -80z" />
+                        <path d="M880 1440 l0 -80 560 0 560 0 0 80 0 80 -560 0 -560 0 0 -80z" />
+                        <path d="M400 1120 l0 -80 160 0 160 0 0 80 0 80 -160 0 -160 0 0 -80z" />
+                        <path d="M880 1120 l0 -80 560 0 560 0 0 80 0 80 -560 0 -560 0 0 -80z" />
                         <path d="M3225 2226 c-137 -45 -243 -137 -303 -263 -37 -77 -37 -79 -37 -203
                             0 -124 0 -126 37 -203 64 -135 186 -234 327 -267 33 -8 89 -11 147 -8 80 4
                             103 10 169 41 103 49 183 129 233 234 37 77 37 79 37 203 0 124 0 126 -37 203
                             -51 105 -130 185 -235 235 -74 35 -84 37 -188 39 -70 1 -125 -3 -150 -11z
                             m263 -176 c62 -28 136 -101 163 -164 32 -71 32 -181 0 -252 -28 -64 -101 -136
                             -166 -165 -74 -34 -182 -33 -255 2 -202 96 -250 361 -94 513 96 93 232 119
-                            352 66z"/>
+                            352 66z" />
                         <path d="M3785 4455 l-25 -24 0 -831 0 -831 25 -24 24 -25 256 0 255 0 0 -135
                             c0 -131 1 -137 25 -160 15 -16 35 -25 56 -25 29 0 52 20 194 160 l161 160 117
                             0 c112 0 119 1 142 25 l25 24 0 831 0 831 -25 24 -24 25 -591 0 -591 0 -24
                             -25z m1095 -855 l0 -720 -96 0 -97 0 -103 -102 -104 -102 0 78 c0 68 -3 80
-                            -25 101 l-24 25 -256 0 -255 0 0 720 0 720 480 0 480 0 0 -720z"/>
-                        <path d="M4080 4080 l0 -80 320 0 320 0 0 80 0 80 -320 0 -320 0 0 -80z"/>
-                        <path d="M4080 3760 l0 -80 320 0 320 0 0 80 0 80 -320 0 -320 0 0 -80z"/>
-                        <path d="M4080 3440 l0 -80 320 0 320 0 0 80 0 80 -320 0 -320 0 0 -80z"/>
-                        <path d="M4080 3120 l0 -80 320 0 320 0 0 80 0 80 -320 0 -320 0 0 -80z"/>
+                            -25 101 l-24 25 -256 0 -255 0 0 720 0 720 480 0 480 0 0 -720z" />
+                        <path d="M4080 4080 l0 -80 320 0 320 0 0 80 0 80 -320 0 -320 0 0 -80z" />
+                        <path d="M4080 3760 l0 -80 320 0 320 0 0 80 0 80 -320 0 -320 0 0 -80z" />
+                        <path d="M4080 3440 l0 -80 320 0 320 0 0 80 0 80 -320 0 -320 0 0 -80z" />
+                        <path d="M4080 3120 l0 -80 320 0 320 0 0 80 0 80 -320 0 -320 0 0 -80z" />
                     </g>
                 </svg>
             </div>
@@ -105,9 +106,12 @@
         </div>
     </nav>
     <div class="container mx-auto py-10 flex justify-center items-center ">
-        <div class="bg-gray-50 bg-opacity-30 border border-black border-opacity-20 p-3 md:p-10 rounded-lg shadow-lg max-w-2xl">
-            <h1 class="h-14 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500 text-center mb-4 text-2xl font-extrabold leading-none tracking-tight  md:text-3xl lg:text-4xl dark:text-white">Job Vacancy Posting System</h1>
-<!--            student's information-->
+        <div
+            class="bg-gray-50 bg-opacity-30 border border-black border-opacity-20 p-3 md:p-10 rounded-lg shadow-lg max-w-2xl">
+            <h1
+                class="h-14 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500 text-center mb-4 text-2xl font-extrabold leading-none tracking-tight  md:text-3xl lg:text-4xl dark:text-white">
+                Job Vacancy Posting System</h1>
+            <!--            student's information-->
             <p><strong>Name:</strong> Dang Nam Khanh</p>
             <p><strong>Student ID:</strong> 104225661</p>
             <p><strong>Email:</strong> <a href="">104225661@student.swin.edu.au</a></p>
@@ -116,10 +120,16 @@
                 from any other student’s work or from any other source.</p>
             <br>
             <?php
-            require_once ("settings.php");
+            require_once("settings.php");
             $conn = @mysqli_connect($host, $user, $pswd);
-            @mysqli_select_db($conn, 's104225661_db')
-            or die('Database not available');
+            if ($conn === false) {
+                die("Error: Unable to connect. " . mysqli_connect_error());
+            }
+
+            if (!@mysqli_select_db($conn, $dbnm)) {
+                die("Error: Unable to select database. " . mysqli_error($conn));
+            }
+            // or die('Database not available');
             if (!$conn) {
                 echo "<p>Database connection failure</p>";
             } else {
@@ -136,13 +146,13 @@
                 if ($result1) {
                     echo "<p>Table 'friends' created successfully</p>";
                 } else {
-                    echo "<p>Table 'friends' creation failure." .  mysqli_error($conn) . "</p>";
+                    echo "<p>Table 'friends' creation failure." . mysqli_error($conn) . "</p>";
                 }
 
 
                 $sql_create_second_table = "CREATE TABLE IF NOT EXISTS myfriends (
                     friend_id1 INT NOT NULL,
-                    friend_id2 INT NOT NULL,
+                    friend_id2 INT NOT NULL
                 );";
 
 
@@ -150,17 +160,16 @@
                 if ($result2) {
                     echo "<p>Table 'myfriends' created successfully</p>";
                 } else {
-                    echo "<p>Table 'myfriends' creation failure." .  mysqli_error($conn) . "</p>";
+                    echo "<p>Table 'myfriends' creation failure." . mysqli_error($conn) . "</p>";
                 }
 
 
                 $query_first_table = "SELECT * FROM friends";
                 $result3 = mysqli_query($conn, $query_first_table);
                 if ($result3) {
-                    if (mysqli_num_rows($result3) > 0){
+                    if (mysqli_num_rows($result3) > 0) {
                         echo "<p>Table 'friends' has records</p>";
-                    }
-                    else {
+                    } else {
                         $sql_add_friends = "INSERT INTO friends (friend_email, password, profile_name, date_started, num_of_friends) VALUES
                             ('john@example.com', 'password123', 'John Doe', '2023-01-15', 25),
                             ('alice@example.com', 'alicepass', 'Alice Smith', '2023-02-28', 30),
@@ -184,9 +193,40 @@
 
                 $query_second_table = "SELECT * FROM myfriends";
                 $result5 = mysqli_query($conn, $query_second_table);
-                if ($result5){
-                    if (mysqli_num_rows($result5) > 0){
+                if ($result5) {
+                    if (mysqli_num_rows($result5) > 0) {
                         echo "<p>Table 'myfriends' has records</p>";
+                    } else {
+                        $sql_add_myfriends = "INSERT INTO myfriends (friend_id1, friend_id2) VALUES
+                        (1,2),
+                        (1,3),
+                        (1,4),
+                        (1,5),
+                        (1,6),
+                        (1,7),
+                        (1,8),
+                        (1,9),
+                        (1,10),
+                        (2,3),
+                        (2,4),
+                        (2,5),
+                        (2,6),
+                        (2,7),
+                        (2,8),
+                        (2,9),
+                        (2,10),
+                        (3,4),
+                        (3,5),
+                        (3,6),
+                        (3,7),
+                        (3,8),
+                        (3,9);";
+                        $result6 = mysqli_query($conn, $sql_add_myfriends);
+                        if (!$result6) {
+                            echo "<p>Records addition to 'myfriends' table failure." . mysqli_error($conn) . "</p>";
+                        } else {
+                            echo "<p>Records added to 'myfriends' table successfully</p>";
+                        }
                     }
                 }
 
@@ -197,11 +237,15 @@
 
 
             ?>
-            <a href="signup.php" class="underline text-blue-700 block mt-3 w-60">Sign up <span class="text-xl ">&#x203A</span> </a>
-            <a href="login.php" class="underline text-blue-700 block mt-3 w-60">Login <span class="text-xl">&#x203A</span> </a>
-            <a href="about.php" class="underline text-blue-700 block mt-3 w-60">About <span class="text-xl">&#x203A</span> </a>
+            <a href="signup.php" class="underline text-blue-700 block mt-3 w-60">Sign up <span
+                    class="text-xl ">&#x203A</span> </a>
+            <a href="login.php" class="underline text-blue-700 block mt-3 w-60">Login <span
+                    class="text-xl">&#x203A</span> </a>
+            <a href="about.php" class="underline text-blue-700 block mt-3 w-60">About <span
+                    class="text-xl">&#x203A</span> </a>
 
         </div>
     </div>
 </body>
+
 </html>
